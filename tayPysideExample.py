99f44164a72e47eb27d5d3dc3212c8a04df999d0
tayarndt <tarndt@accessible360.com>
2022-02-07 17:58:11 -0600

    added menubar

--- tayPysideExample.py
+++ tayPysideExample.py
@@ -2,15 +2,14 @@ from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 import sys
-class Main(QWidget):
+class Window(QMainWindow):
     def __init__(self):
-        super(Main, self).__init__()
+        super(Window, self).__init__()
         self.initUI()
     def initUI(self):
         self.setWindowTitle("TayPyside Example")
         self.grid = QGridLayout(self)
         self.setLayout(self.grid)
-        self.setGeometry(300, 300, 300, 300)
         self.welcomeText = QLabel(self)
         self.welcomeText.setFocus="true"
         self.welcomeText.setText("Welcome to the PySide6 application. This application demonstrates the power ofPySide6 for making accessible GUI apps with python. Press next to continue.")
@@ -19,8 +18,20 @@ class Main(QWidget):
         self.nextButton.setText("Next")
         self.nextButton.setShortcut("Alt+N")
         self.grid.addWidget(self.nextButton,1,2)
+        applicationMenu = self.menuBar()
+        file = applicationMenu.addMenu("File")
+        file.addAction("New")
+        save = QAction("Save",self)
+        save.setShortcut("Ctrl+S")
+        file.addAction(save)
+        edit = applicationMenu.addMenu("Edit")
+        edit.addAction("copy")
+        edit.addAction("paste")
+        quit = QAction("Quit",self) 
+        file.addAction(quit)
+        self.setMenuBar(applicationMenu)
         self.show()
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main = Main()
+    window = Window()
     sys.exit(app.exec_())
